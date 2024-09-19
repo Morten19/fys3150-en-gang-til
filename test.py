8d5e4bde6fd1fdf41ea1f7e98780706040190a09
Morten19 <morten.soerland2@gmail.com>
2024-09-19 18:07:31 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -1,21 +1,48 @@
 
 
 
-
 import numpy as np
-import matplotlib.pyplot as plt
+
+
+a=([2,-1,0,0,0])
+b=([-1,2,-1,0,0])
+c=([0,-1,2,-1,0])
+e=([0,0,-1,2,-1])
+f=([0,0,0,-1,2])
+
+h=([1,1.6,1.8,1.6,1])
+
+g=np.array([a,b,c,e,f])
+
+
+
+def funk(x,y):
+    return (np.dot(x,y))
+
+
+print(funk(g,h))
+
+#python3 test.py
+#[0.4 0.4 0.4 0.4 0.4]
 
 
 
 
-e=np.loadtxt("data.txt")
 
-f=(e[:,0])
 
-g=(e[:,1])
 
-plt.plot(f,g)
-plt.xlabel("t")
-plt.ylabel("f(t)")
-plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
